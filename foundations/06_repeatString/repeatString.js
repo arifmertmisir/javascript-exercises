c8d3942a5c0bfbb1d14db4a28cb61e6fd18009f8
Mert Misir <167693183+arifmertmisir@users.noreply.github.com>
2026-06-10 15:03:20 +0200

    complete foundation exercises until tempConversion

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  if (num === 0) {
+    return "";
+  }
+
+  let temp = string;
+  for (let i = 1; i <= num - 1; i++) {
+    string = string + temp;
+  }
+  return string;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (from, until) {
+  if (from < 0 || until < 0) {
+    return "ERROR";
+  }
 
+  if (!Number.isInteger(from) || !Number.isInteger(until)) {
+    return "ERROR";
+  }
+
+  let total = 0;
+  if (from < until) {
+    for (let i = from; i <= until; i++) {
+      total += i;
+    }
+    return total;
+  } else {
+    for (let i = from; i >= until; i--) {
+      total += i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
